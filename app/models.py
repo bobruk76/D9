408a518464c46835930e14b0461b33d967cb9c4e
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-07-28 17:27:51 +0300

    add model

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    slug = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
+    post_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name} ({self.slug})'
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField
+    status = models.CharField(max_length=10, choices=[('D', 'draft'), ('P', 'published')])
+    content = models.TextField()
+    updated = models.DateTimeField(default=timezone.now)
+    publication_date = models.DateTimeField(default=timezone.now)
+    category = models.ManyToManyField(Category, blank=True)
